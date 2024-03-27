23c083148c7bfcf57e2e78993f9cc15499a8f8bb
Riya <riya@gmail.com>
2024-03-27 17:08:49 +0530

    Model is not shown.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,25 +1,91 @@
-$(document).ready(function () {
+ $.validator.addMethod("minAge", function(value, element, min) {
+     var today = new Date();
+     var birthDate = new Date(value);
+     var age = today.getFullYear() - birthDate.getFullYear();
 
-    // Validate Username
-    $("#usercheck").hide();
-    let usernameError = true;
-    $("#name").keyup(function () {
-        validateUsername();
-    });
+     if (age > min+1) {
+         return true;
+     }
 
-    function validateUsername() {
-        let usernameValue = $("#name").val();
-        if (usernameValue.length == "") {
-            $("#usercheck").show();
-            usernameError = false;
-            return false;
-        } else if (usernameValue.length < 3 || usernameValue.length > 10) {
-            $("#usercheck").show();
-            $("#usercheck").html("**length of username must be between 3 and 10");
-            usernameError = false;
-            return false;
-        } else {
-            $("#usercheck").hide();
-        }
-    }
-});
+     var m = today.getMonth() - birthDate.getMonth();
+
+     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+         age--;
+     }
+
+     return age >= min;
+     },
+ );
+
+ $.validator.addMethod(
+     "regex",
+     function(value, element, regexp) {
+         var check = false;
+         var re = new RegExp(regexp);
+         return this.optional(element) || re.test(value);
+     },
+     "Special char not allowed!"
+ );
+
+$(document).ready(function() {
+ $("#registrationForm").validate({
+     rules: {
+         'userName': {
+         required: true,
+         minlength: 6,
+         maxlength: 20,
+         },
+
+         gender:{
+            required: true
+         },
+
+         'dateOfBirth': {
+         minAge: 18
+         },
+
+         'phoneNumber': {
+          required: true,
+          minlength: 10,
+          maxlength: 10
+         },
+
+         'biography': {
+         maxlength: 50
+         }
+     },
+
+     messages: {
+         userName: {
+         required: "Fill Username!",
+         maxlength: "Username contains 20 characters!",
+         minlength: "Minimum 6 characters required!"
+         },
+
+         gender: {
+         required: "Select your gender!"
+         },
+
+         dateOfBirth: {
+         minAge: "age should be 18!"
+         },
+
+         biography: {
+         maxlength: "Only 50 chars allowed!"
+         },
+
+         phoneNumber: {
+         required: "Fill Phone Field",
+         maxlength: "Only 10 allowed!"
+         }
+     },
+
+     highlight: function (element) {
+         $(element).closest('.control-group').removeClass('success').addClass('error');
+     },
+
+     submitHandler: function(form) {
+         form.submit();
+     }
+   });
+ });
--- src/main/resources/static/js/mapping.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8086/api/v1/actors"
-    }).then(function(data) {
-       $('.greeting-id').append(data.id);
-       $('.greeting-content').append(data.content);
-    });
-});
